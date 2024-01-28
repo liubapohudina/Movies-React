864f21644f95e6ece7f0282cb566ea0af05da2bd
Liubov <drogaltseva92@gmail.com>
2024-01-28 12:00:10 +0100

    fix error for img

--- src/components/Casts/CastItem/CastItem.jsx
+++ src/components/Casts/CastItem/CastItem.jsx
@@ -4,7 +4,11 @@ const CastItem = ({ datacast }) => {
     const PATH_IMAGE = 'https://image.tmdb.org/t/p/w500';
     const element = datacast.map(({ id, original_name, character, profile_path }) => (
         <li id={id} key={id}>
-            <img src={PATH_IMAGE + profile_path} alt={character} width='300' height='450' loading="lazy"/>
+              {profile_path ? (
+                <img src={PATH_IMAGE + profile_path} alt={character} width='300' height='450' loading="lazy"/>
+            ) : (
+                <div className={styles.error}>Sorry, the path to image is broken or ampty</div>
+            )}
             <h2>{original_name}</h2>
             <h3>{character}</h3>
         </li>
